5cf88b9b162c4d76146477ab7b0ee62a8ab9b212
BHASKAR BISWAS <73770735+bhaskarbiswas04@users.noreply.github.com>
2026-03-20 21:29:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>Chat  2 Go</h1>
+      </>
+  )
+}
+
+export default App
