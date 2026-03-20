b2fc6fc6e3116c12b981806820d51c16206a6d90
BHASKAR BISWAS <73770735+bhaskarbiswas04@users.noreply.github.com>
2026-03-20 22:42:36 +0530

    updt: component created and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import { useState } from 'react'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import './App.css'
 
+import HomePage from "./pages/HomePage";
+import SignUp from "./components/SignUp";
+import LogIn from "./components/LogIn";
+
+const router = createBrowserRouter([
+  {path: "/", element: <HomePage />},
+  {path: "/signup", element: <SignUp />},
+  {path: "/login", element: <LogIn />},
+
+])
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-    <h1>Chat  2 Go</h1>
-      </>
-  )
+    <div className="p-4 h-screen flex items-center justify-center">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App
+
---
+++ src/components/SignUp.jsx
@@ -0,0 +1,9 @@
+
+
+export default function SignUp() {
+    return (
+      <div>
+        <h1>Signup Page</h1>
+      </div>
+    );
+}
