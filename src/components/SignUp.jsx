59ce27157ac5db8d8fa7ae414d4c968338a5cd10
BHASKAR BISWAS <73770735+bhaskarbiswas04@users.noreply.github.com>
2026-03-21 14:22:34 +0530

    updt: routing and tailwind

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,9 +1,12 @@
-
-
 export default function SignUp() {
-    return (
-      <div>
-        <h1>Signup Page</h1>
+  return (
+    <div>
+      <div
+        className="p-4 bg-gray-0 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 border border-gray-100"
+      >
+        <h1 className="text-3xl font-bold text-center">Sign Up</h1>
+        
       </div>
-    );
+    </div>
+  );
 }
