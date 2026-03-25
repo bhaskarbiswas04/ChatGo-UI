80247e1e1b29d298e5646b35d04bf525c77e3690
BHASKAR BISWAS <73770735+bhaskarbiswas04@users.noreply.github.com>
2026-03-25 10:00:30 +0530

    updt: building sidebar

---
+++ src/components/MessageContainer.jsx
@@ -0,0 +1,8 @@
+
+export default function MessageContainer () {
+    return (
+      <div>
+        <h1>MessageContainer</h1>
+      </div>
+    );
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
-
+import MessageContainer from "../components/MessageContainer";
+import Sidebar from "../components/Sidebar";
 
 export default function HomePage() {
-    return(
-        <div>
-            <h1>Home Page</h1>
-        </div>
-    )
+    return (
+      <div className="flex sm:h-112.5 md:h-137.5 rounded-lg overflow-hidden bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+        <Sidebar />
+        <MessageContainer />
+      </div>
+    );
 }
