530f5507903bc185447fc7c218a275211d4bde95
BHASKAR BISWAS <73770735+bhaskarbiswas04@users.noreply.github.com>
2026-03-26 10:08:09 +0530

    building sendMessage

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,8 +2,23 @@ import React from 'react'
 
 function Message() {
   return (
-    <div>Message</div>
-  )
+    <div className="chat chat-end">
+      <div className="chat-image avatar">
+        <div className="w-10 rounded-full">
+          <img
+            alt="Tailwind CSS chat bubble component"
+            src="https://img.daisyui.com/images/profile/demo/kenobee@192.webp"
+          />
+        </div>
+      </div>
+      <div className="chat-footer mt-2">
+        <time className="text-xs opacity-50">12:45</time>
+      </div>
+      <div className="chat-bubble chat-bubble-accent">
+        You were the Chosen One!
+      </div>
+    </div>
+  );
 }
 
 export default Message
--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -2,9 +2,7 @@ import Message from "../components/Message"
 
 function Messages() {
   return (
-    <div className="px-4 flex-1 overflow-auto">
-        <Message />
-        <Message />
+    <div className="p-4 flex-1 overflow-auto">
         <Message />
         <Message />
         <Message />
