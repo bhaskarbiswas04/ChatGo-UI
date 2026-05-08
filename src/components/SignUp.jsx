56dbca62d7186131da2d0dd5ca9ccb9a29afe12c
BHASKAR BISWAS <73770735+bhaskarbiswas04@users.noreply.github.com>
2026-05-08 10:28:09 +0530

    updt: cors policy improved

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -28,7 +28,6 @@ export default function SignUp() {
           headers: {
             "Content-Type": "application/json",
           },
-          withCredentials: true,
         },
       );
       
